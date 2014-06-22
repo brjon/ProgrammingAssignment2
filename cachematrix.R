43d036445cd57321637fe78eed59173d8ce508dc
consulting-bh <brenner2@gmail.com>
2014-06-22 17:38:53 +0300

    Comments for cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,16 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions in this code combine to:
+## -create a matrix object
+## -allow access to the object in order to set the value, retrieve the value.
+## -calculate the inverse of the matrix and store the result in a "cache" variable
+## -If the inverse has already been calculated, the previous result will be retrieved
+##  without being recalculated, so as to save on processing time
+## The functions are called by e.g.
+## 1) selecting a matrix to invert :mat<-rbind(1:3,c(0,1,5),c(5,6,0))
+## 2) calling CacheMatrix<-makeCacheMatrix(mat)
+## 3) calling cacheSolve(CacheMatrix)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## makeCacheMatrix creates a special "matrix" and stores the matrix as well as its inverse
+## The function is a list of four functions, which are evaluated when called by the cacheSolve function 
+## The set and setinverse functions return the the matrix and inverse of the matrix respectively 
